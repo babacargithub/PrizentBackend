7c49dc1d3c6824be74ad2977c1ce31e8669b916e
babacargithub <46243872+babacargithub@users.noreply.github.com>
2023-05-20 12:25:37 +0000

    renamed models

--- app/Models/Formule.php
+++ app/Models/Formule.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Horaire extends Model
+class Formule extends Model
 {
     use HasFactory;
 }
